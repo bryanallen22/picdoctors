cd1f74b264eeecd07189810793bd10b7a5ab2fc0
Bryan Allen <bryanallen22@gmail.com>
2013-01-12 02:49:22 -0700

    Adds ability to add a bank account
    Bank accounts can be added to doctor accounts now. This funcitonality is
    still incomplete, however:
    
     - Successful bank account adds are silent (no refresh even)
     - Need to add support for #tabid in url for settings page
     - Need to fix look and feel of bank account settings page
     - Need to add deletion of bank accounts
    
    Well, if this commit is lacking so much, why am I pushing it already? So
    I can feel like I'm working on this stuff again.

--- doctor/account_settings_views.py
+++ doctor/account_settings_views.py
@@ -1,27 +1,42 @@
 # Create your views here.
 from django.contrib.auth.decorators import login_required
+from django.utils import simplejson
+from django.http import HttpResponse
 
 from annoying.decorators import render_to
 
+from common.functions import get_profile_or_None
+from common.models import BPAccountWrapper
+
+import balanced
 import settings
 
 @login_required
 def create_bank_account(request):
     profile = get_profile_or_None(request)
+    email_address = request.user.email
     if not profile or not profile.is_doctor:
         return HttpResponse('{ }', mimetype='application/json')
-    
-    if profile.bp_account_wrapper:
-        raise KeyError("Can't create a new account for %s - he/she already has" \
-                       " bp_account_wrapper.id=%s" % request.user.email, profile.bp_account_wrapper.id)
 
     try:
-        account = balanced.Marketplace.my_marketplace.create_merchant(email_address, 
-                              bank_account_uri=request.POST['bank_account_uri'])
-        wrapper = BPAccountWrapper(uri=account.uri)
-        wrapper.save()
-        profile.bp_account_wrapper = wrapper
-        profile.save()
+
+        # Configure balanced
+        balanced.configure(settings.BALANCED_API_KEY_SECRET)
+        
+        # Get their account if they have one
+        if profile.bp_account_wrapper:
+            account = balanced.Account.find(profile.bp_account_wrapper.uri)
+        else:
+            # Create a new account and associate it with this profile
+            account = balanced.Account().save()
+            wrapper = BPAccountWrapper(uri=account.uri)
+            wrapper.save()
+            profile.bp_account_wrapper = wrapper
+            profile.save()
+
+        # Add the bank account to the user's account
+        account.add_bank_account(request.POST['bank_account_uri'])
+
     except balanced.exc.HTTPError as ex:
         if ex.category_code == 'duplicate-email-address':
             raise KeyError("Duplicate email address %s... this should have been saved under " \
@@ -30,7 +45,7 @@ def create_bank_account(request):
             raise
 
     # TODO add bank stuff here
-    ret = {}
+    ret = { "success" : True }
     response_data = simplejson.dumps(ret)
     return HttpResponse(response_data, mimetype='application/json')
 
@@ -38,7 +53,16 @@ def create_bank_account(request):
 @login_required
 @render_to('account_settings_doc.html')
 def settings_doc(request):
+    profile = get_profile_or_None(request)
+    balanced.configure(settings.BALANCED_API_KEY_SECRET)
+    if profile.bp_account_wrapper:
+        account = balanced.BankAccount.find(profile.bp_account_wrapper.uri)
+        bank_accounts = [ba for ba in account.bank_accounts]
+    else:
+        bank_accounts = None
+
     return { 
         'marketplace_uri'   : settings.BALANCED_MARKETPLACE_URI,
+        'bank_accounts'     : bank_accounts,
     }
 
--- static/js/account_settings.js
+++ static/js/account_settings.js
@@ -3,65 +3,60 @@
 
 // Load the application once the DOM is ready, using `jQuery.ready`:
 $(function(){
-  /* .user_card doesn't exist on doctor settings pages, but that's okay */
-  /* Removed -- for now, we're just processing the cards as part of the normal checkout flow
-  $('.user_card button').click( function(e) {
-    e.preventDefault();
+  /* uri comes directly from template */
+  balanced.init(marketplace_uri);
 
-    // Prevent them from clicking again
-    $(this).attr('disabled', 'disabled');
+  var $form = $('#bank-account-form');
 
+  function submitToken(bank_account_uri) {
     var CSRF_TOKEN = $('input[name=csrfmiddlewaretoken]').attr('value');
-    var card_div = $(this).closest('.user_card');
-    var card_uri = card_div.find('.uri').text();
-    var obj = { "card_uri" : card_uri };
+
+    var obj = { "bank_account_uri" : bank_account_uri };
 
     $.ajax({
       headers: {
         "X-CSRFToken":CSRF_TOKEN
       },
       type: "POST",
-      url: '/account_settings_delete_card/',
+      url: '/create_bank_account/',
       data: obj,
       success : function(data, textStatus) {
+          debugger
         if ( data.success ) {
-          card_div.remove();
+          // reload page?
         }
         else {
           // Silently fail to do anything if we are unable to delete.
           // REVISIT
-
-          // I suppose we can at least let them try again?
-          $(this).removeAttr('disabled');
         }
       },
     });
-  });
-  */
 
-  /* uri comes directly from template */
-  balanced.init(marketplace_uri);
-
-  var $form = $('#bank-account-form');
+  }
 
   function callbackHandler(response) {
     switch (response.status) {
       case 201:
         // WOO HOO!
         // response.data.uri == uri of the card or bank account resource
+        submitToken(response.data.uri);
         break;
+
       case 400:
         // missing field - check response.error for details
-        break;
+        //break;
       case 402:
         // we couldn't authorize the buyer's credit card
         // check response.error for details
-        break
+        //break;
       case 404:
         // your marketplace URI is incorrect
-        break;
+        //break;
       case 500:
         // Balanced did something bad, please retry the request
+        //break;
+      default:
+        $('#error_bankaccount').show();
         break;
     }
   }
@@ -74,8 +69,6 @@ $(function(){
        "type": $form.find("select option:selected").val(),
     }
 
-    debugger
-
     if( !balanced.bankAccount.validateRoutingNumber( bankAccountData.routing_number) )
     {
       $("#routing-error").show();
@@ -85,18 +78,17 @@ $(function(){
     if( !balanced.bankAccount.validate({
            'routing_number' : bankAccountData.routing_number,
            'account_number' : bankAccountData.account_number,
-           'jk' : bankAccountData.name }))
+           'name'           : bankAccountData.name }))
     {
-      debugger
+      $('#error_bankaccount').show();
     }
-
-    debugger
 
     balanced.bankAccount.create(bankAccountData, callbackHandler);
   }
 
   $form.find("button:submit").click( function(e) {
     e.preventDefault();
+    $('#error_bankaccount').hide();
     createAcct();
   });
 
