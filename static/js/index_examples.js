73baed4a6a52b830ae62c3b1dff433b9ff539ccf
Bryan Allen <bryanallen22@gmail.com>
2013-02-28 13:03:30 -0700

    Makes the index page look pretty
    I'm not sure how this work on decrepit browsers. I need to check.

--- static/js/index_examples.js
+++ static/js/index_examples.js
@@ -1,27 +1,10 @@
 // Load the application once the DOM is ready, using `jQuery.ready`:
 $(function(){
 
-  var cur_row = 0;
-  var delay = 8000;
+  $('.carousel').carousel({
+      interval: 8000,
+      pause: null, /* Don't pause when they hover over it */
 
-  var toggleNext = function() {
-    var example_rows = $(".examplerow");
-
-    var next_row = cur_row + 1;
-    if( next_row >= example_rows.length ) {
-      next_row = 0;
-    }
-
-    $(example_rows[cur_row]).fadeOut( function() {
-      $(example_rows[next_row]).fadeIn( function() {
-        cur_row = next_row;
-        window.setTimeout( toggleNext, delay );
-      });
-    });
-
-  }
-
-  window.setTimeout( toggleNext, delay );
-
+  });
 
 });
