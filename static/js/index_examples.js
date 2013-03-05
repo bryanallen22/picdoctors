f0912e8315b61292b8a4766e7e8ba96058c6cbad
Bryan Allen <bryanallen22@gmail.com>
2013-03-05 00:22:27 -0700

    Index hero unit mouseenter stops cycling of pics

--- static/js/index_examples.js
+++ static/js/index_examples.js
@@ -1,10 +1,22 @@
 // Load the application once the DOM is ready, using `jQuery.ready`:
 $(function(){
 
-  $('.carousel').carousel({
+  $('#myCarousel').carousel({
       interval: 8000,
       pause: null, /* Don't pause when they hover over it */
 
+  });
+
+  $('.hero-unit').mouseenter( function() {
+    $('#myCarousel').carousel( 'pause' );
+  });
+
+  $('.hero-unit').mouseleave( function() {
+    /* So, this is annoying. Doing this resets the time, so if you
+     * briefly hover on the picture it makes that cycle seem a lot
+     * longer. Unfortunately, there's no better way exposed unless
+     * I want to modify bootstrap itself. Which I don't want to now. */
+    $('#myCarousel').carousel( 'cycle' );
   });
 
 });
