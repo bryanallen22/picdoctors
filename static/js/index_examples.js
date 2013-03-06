34b71b299115958c56c58d0298f7b265d10a4ddf
Bryan Allen <bryanallen22@gmail.com>
2013-03-06 14:34:34 -0700

    Add restoration example to index.html
    This commit also lines up botanical before/after pictures.  I like it
    fading from before to after without changing the orientation or cropping
    at all. It makes for a pretty cool fade in effect.
    
    This also makes the hero-unit separate from the transitions so it can
    always be clicked, but the transitions still happened. Previously we
    reset our counter every time and could click during transitions, so it
    was really ugly. This is much better.

--- static/js/index_examples.js
+++ static/js/index_examples.js
@@ -7,16 +7,4 @@ $(function(){
 
   });
 
-  $('.hero-unit').mouseenter( function() {
-    $('#myCarousel').carousel( 'pause' );
-  });
-
-  $('.hero-unit').mouseleave( function() {
-    /* So, this is annoying. Doing this resets the time, so if you
-     * briefly hover on the picture it makes that cycle seem a lot
-     * longer. Unfortunately, there's no better way exposed unless
-     * I want to modify bootstrap itself. Which I don't want to now. */
-    $('#myCarousel').carousel( 'cycle' );
-  });
-
 });
