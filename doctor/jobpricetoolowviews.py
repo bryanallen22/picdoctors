1df9743a411efcea6db70a4b3ac7d5cd61e1c5a4
kingpin2k <kingpin2k@gmail.com>
2013-03-16 22:38:45 -0600

    Finished Job Price Too Low
    Email the User every 5 doctors that say the job is too low
    
    I might in the future push the job down lower on the totem poll or
    something random like that.  I dunno

--- doctor/jobpricetoolowviews.py
+++ doctor/jobpricetoolowviews.py
@@ -10,6 +10,10 @@ from annoying.decorators import render_to
 from common.models import *
 from common.functions import get_profile_or_None
 from common.balancedfunctions import get_merchant_account, get_withdraw_jobs, is_merchant, credit_doctor
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from tasks.tasks import sendAsyncEmail
 
 import settings
 import ipdb
@@ -49,3 +53,48 @@ def job_price_too_low_action(request, job):
                     price = max(job.bp_hold.cents, int(float(request.POST['price'])*100))
                     contrib=PriceTooLowContributor(job=job, doctor=doc, price=price)
                     contrib.save()
+                    
+                if job.price_too_low_count % 5 == 0:
+                    send_user_email(job)
+
+def send_user_email(job):
+    try:
+        to_email = [job.skaa.email]
+
+        subject = "Job price too low"
+
+        contribs = PriceTooLowContributor.objects.filter(job=job.id)
+        avg_price = 0
+
+        for contrib in contribs:
+            avg_price += contrib.price
+
+        avg_price = avg_price / len(contribs)
+        avg_price = float(avg_price) / 100
+        site_path = reverse('job_page_with_page_and_id', args=[1, job.id])
+
+
+        args = {
+                'avg_price'             : avg_price,
+                'job_id'                : job.id,
+                'number_of_doctors'     : len(contribs),
+                'site_url'              : settings.SITE_URL,
+                'site_path'             : site_path,
+                } 
+        html_content = render_to_string('job_price_too_low_email.html', args)
+                                        
+        
+        # this strips the html, so people will have the text
+        text_content = strip_tags(html_content) 
+        # create the email, and attach the HTML version as well.
+        msg = EmailMultiAlternatives(subject, text_content, 'donotreply@picdoctors.com', to_email)
+        msg.attach_alternative(html_content, "text/html")
+        #TODO if you want to switch to using the workers
+        # sendAsyncEmail.apply_async(args=[msg])
+        sendAsyncEmail(msg)
+
+        return True
+    except Exception as ex:
+        return False
+
+
