e0ef716a3911256e097ac070e4fd8b55b8ef4bbf
Bryan Allen <bryanallen22@gmail.com>
2013-03-28 01:54:20 -0600

    Move AWS stuff to admin@picdoctors.com account
    I've revoked my personal account stuff and tied everything to
    admin@picdoctors.com, which is tied to our company credit card.

--- settings/dev.py
+++ settings/dev.py
@@ -6,9 +6,9 @@ IS_PRODUCTION = False # used to toggle things that don't go on the non-live site
 BALANCED_API_KEY_SECRET = '2726feb439f011e294b1026ba7f8ec28'
 BALANCED_MARKETPLACE_URI = '/v1/marketplaces/TEST-MP1c0n2GjbjUEgctn32jYQE0'
 
-AWS_ACCESS_KEY_ID = 'AKIAIXCLPXQHNZTMBRXQ'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'picdoctors'
+AWS_ACCESS_KEY_ID = 'AKIAJYVA2NHBM5YJ3WWQ'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'picdoctors_nonproduction'
 
 DATABASES = {
     'default': {
--- settings/production.py
+++ settings/production.py
@@ -10,5 +10,9 @@ DATABASES = {
     }
 }
 
+AWS_ACCESS_KEY_ID = 'AKIAJ5JN3Q5MTH5LX6QQ'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'picdoctors_media'
+
 # apparently DJANGO isn't smart enough to figure out https if you are using a proxy to fake https
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTOCOL', 'https')
