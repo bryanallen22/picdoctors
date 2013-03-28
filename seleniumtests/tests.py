70ca838cfc4a72d62f413c0610ee3b110172f169
Bryan Allen <bryanallen22@gmail.com>
2013-03-28 15:17:24 -0600

    A few selenium changes
    Not much here, but it's bugging me to remain uncommitted. I've got beef
    this up a little for 2 reasons:
    1) I'm sick of recreating users
    2) Automated sanity will let me be less scared of checkins when we are
    live

--- seleniumtests/tests.py
+++ seleniumtests/tests.py
@@ -8,6 +8,7 @@ Replace this with more appropriate tests for your application.
 #from django.test import TestCase
 from django.utils import unittest
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 
 import settings
 from settings.functions import get_cfg_setting
@@ -31,12 +32,18 @@ class CreateSkaa(unittest.TestCase):
         driver.get( self.domain )
         self.assertIn("PicDoctors", driver.title)
 
+        ######
+        # Home page
+        ######
         # Hide django toolbar
         driver.find_element_by_id("djHideToolBarButton").click()
 
         # Find the "Upload" button and click it
         driver.find_element_by_id("upload_button").click()
 
+        ######
+        # Upload page
+        ######
         # upload an actual file
         el = driver.find_element_by_id("fileinput")
         el.send_keys(
@@ -45,6 +52,14 @@ class CreateSkaa(unittest.TestCase):
 
         # Move on to the markup page
         driver.find_element_by_id("next").click()
+
+        ######
+        # Markup page
+        ######
+        action_chains = ActionChains(driver)
+        el = driver.find_element_by_class_name("markup_pic_container")
+        action_chains.drag_and_drop_by_offset(el, 100, 50)
+        action_chains.perform()
 
         import ipdb; ipdb.set_trace()
 
