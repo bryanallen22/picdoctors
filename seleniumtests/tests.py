e7f912e4ebbbd1437031bec391e0c90155fa6cb3
Bryan Allen <bryanallen22@gmail.com>
2013-04-03 00:20:13 -0600

    Finish basic user test
    Creates a very simple job

--- seleniumtests/tests.py
+++ seleniumtests/tests.py
@@ -16,19 +16,25 @@ from settings.functions import get_cfg_setting
 import os
 import time
 
-def get_setting(name):
-    return get_cfg_setting("seleniumtests/localsettings.cfg", name)
+def get_setting(name, default):
+    return get_cfg_setting("seleniumtests/localsettings.cfg", name) or default
+
+def set_input(driver, el_id, text):
+    el = driver.find_element_by_id( el_id )
+    el.click()
+    el.send_keys( text )
 
 class CreateSkaa(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30) # can take up to 30 seconds to do something
-        self.email = get_setting('skaa_email')
-        self.domain = get_setting('domain')
+        self.email = get_setting('skaa_email', 'bryan+user@picdoctors.com')
+        self.domain = get_setting('domain', 'http://localhost:8000')
 
     def test_create_user_and_job(self):
         driver = self.driver
+        print "Going to %s" % self.domain
         driver.get( self.domain )
         self.assertIn("PicDoctors", driver.title)
 
@@ -56,12 +62,39 @@ class CreateSkaa(unittest.TestCase):
         ######
         # Markup page
         ######
-        action_chains = ActionChains(driver)
-        el = driver.find_element_by_class_name("markup_pic_container")
-        action_chains.drag_and_drop_by_offset(el, 100, 50)
-        action_chains.perform()
 
-        import ipdb; ipdb.set_trace()
+        # Create a markup. This doesn't work. I don't know why.
+        # I'll have to come back to it.
+        #action_chains = ActionChains(driver)
+        #el = driver.find_element_by_class_name("markup_pic_container")
+        #action_chains.drag_and_drop_by_offset(el, 100, 50)
+        #action_chains.perform()
+
+        # Add a general description
+        el = driver.find_element_by_class_name( 'desc' )
+        el.click()
+        el.send_keys( 'some description' )
+
+        # Click "next"
+        driver.find_element_by_id('next').click()
+
+        ######
+        # Sign In page
+        ######
+        set_input(driver, 'email', self.email )
+        set_input(driver, 'password', 'asdf')
+        set_input(driver, 'confirm_password', 'asdf')
+
+        driver.find_element_by_id("btnsubmit").click()
+
+        ######
+        # Sign In page
+        ######
+        set_input(driver, 'price', '4.00')
+        # Fill in test card info:
+        driver.find_element_by_id( 'btn-visa' ).click()
+        # Submit the payment
+        driver.find_element_by_id('submit_payment').click()
 
     def tearDown(self):
         self.driver.close()
