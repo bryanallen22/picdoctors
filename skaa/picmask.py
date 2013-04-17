232c7d6f80deb5d55fec75572650cdfbfd6f2bf5
Bryan Allen <bryanallen22@gmail.com>
2013-04-17 12:33:42 -0600

    More watermark tweaks
    It's an rectangle with all 4 (rounded) corners now, so the right edge
    doesn't go all the way into the right edge of the picture. I also
    changed the font to be bold, so it's more legible, but it might be too
    bold and obnoxious. Let me know what you think?
    
    I spent considerable time trying to figure out how getty does this
    without any nasty jpeg pixelation fragments around their watermark text.
    I think that they are doing some wild things to the jpeg format, and I'm
    content to just leave it alone for now. It looks crazy.

--- skaa/picmask.py
+++ skaa/picmask.py
@@ -18,8 +18,10 @@ def addOverlayText(im, overlay_text, font, font_size):
     colour = 'white'
     draw = ImageDraw.Draw(im)
     top = overlay_text[0]
-    bottom = overlay_text[1]
+    doc_handle = "  " + overlay_text[1] # use two spaces for indentation
+    bottom = overlay_text[2]
     draw.text((font_size,font_size), top, colour, font=font)
+    draw.text((font_size,int(2.5*font_size)), doc_handle, colour, font=font)
     draw.text((font_size,im.size[1]-font_size*2), bottom, colour, font=font)
 
 def round_corner(radius, fill):
@@ -45,30 +47,38 @@ def watermark(im, mark, position, overlay_text, font, font_size, opacity):
     # watermark in that layer.
     layer = Image.new('RGBA', im.size, (0,0,0,0))
     layer.paste(mark, position)
-    layer.paste(corner, position)
-    layer.paste(corner.rotate(90), 
+    layer.paste(corner, position)  # top left
+    layer.paste(corner.rotate(90), # bottom left
                   (position[0],
+                   position[1]+mark.size[1]-corner_radius))
+    layer.paste(corner.rotate(270), # top right
+                  (position[0]+mark.size[0]-corner_radius,
+                   position[1]))
+    layer.paste(corner.rotate(180),
+                  (position[0]+mark.size[0]-corner_radius,
                    position[1]+mark.size[1]-corner_radius))
     # composite the watermark with the layer
     return Image.composite(layer, im, layer)
 
-mask_ratio = .37
+mask_ratio = .35  # 35% of image width
+right_margin_ratio = 0.03 # 3% of image width
 
-def generate_watermarked_image(im, specific_text):
+def generate_watermarked_image(im, doc_handle):
     im_width = im.size[0]
     im_height = im.size[1]
     mask_width = int(im_width * mask_ratio)
     mask_height = int(im_height * mask_ratio)
     mask_size = (mask_width, mask_height)
+    right_margin = int(im_width * right_margin_ratio)
     mark = Image.new('RGB', mask_size)
     draw = ImageDraw.Draw(mark) # Create a draw object
     draw.rectangle((0, 0, mask_width-1, mask_height-1), fill="black")
-    position = (im_width-mask_width, int(im_height/2))
-    text = (specific_text, "www.PicDoctors.com")
+    position = (im_width-mask_width-right_margin, int(im_height/2))
+    text = ("Done by:", doc_handle, "www.PicDoctors.com")
     opacity = 0.5
     font_size = int(im_height * .035)
     font = ImageFont.truetype(
-        os.path.join(settings.PROJECT_ROOT, "skaa/HelveticaNeueLight.ttf"),
+        os.path.join(settings.PROJECT_ROOT, "skaa/HelveticaBold.ttf"),
         font_size)
     watermarked_image = watermark(im, mark, position, text, font, font_size, opacity)
 #    watermarked_image.show()
