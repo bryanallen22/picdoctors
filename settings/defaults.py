2e851220e72525366ca7ca9f1cf4b2feaf22e6df
Bryan Allen <bryanallen22@gmail.com>
2013-04-26 01:20:17 -0600

    Fix collectstatic

--- settings/defaults.py
+++ settings/defaults.py
@@ -66,12 +66,6 @@ MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media/')
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = '/site_media/'
 
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
-
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
@@ -260,7 +254,7 @@ PIPELINE_CSS = {
             'css/isotope.css',
             'css/qbeforeafter.css'
         ),
-        'output_filename': 'static/all.css',
+        'output_filename': 'all.css',
     },
 }
 
@@ -295,7 +289,15 @@ PIPELINE_JS = {
             ###########
             'js/*.js',
         ),
-        'output_filename': 'static/all.js',
+        'output_filename': 'all.js',
     }
 }
+
+STATICFILES_STORAGE = 'pipeline.storage.PipelineCachedStorage'
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static_out')
 
