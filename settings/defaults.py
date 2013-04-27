6359c3b466b6d2969d45b9a966eb64565829d448
Bryan Allen <bryanallen22@gmail.com>
2013-04-27 01:40:18 -0600

    Remove SHA from URL/path cause that was a dumb idea

--- settings/defaults.py
+++ settings/defaults.py
@@ -291,3 +291,13 @@ PIPELINE_JS = {
 
 STATICFILES_STORAGE = 'pipeline.storage.PipelineCachedStorage'
 
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = '/static/'
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static_out')
+
--- settings/dev.py
+++ settings/dev.py
@@ -31,13 +31,3 @@ import warnings
 import exceptions
 warnings.filterwarnings("ignore", category=exceptions.RuntimeWarning, module='django.db.backends.sqlite3', lineno=53)
 
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static_out')
-
