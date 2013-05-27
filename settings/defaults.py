30e75424be60b8632cf6f7ef6551217b20c60453
Bryan Allen <bryanallen22@gmail.com>
2013-05-27 15:42:28 -0600

    Add Dan back as Admin

--- settings/defaults.py
+++ settings/defaults.py
@@ -11,7 +11,7 @@ TEMPLATE_DEBUG = DEBUG
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
     ('Bryan', 'bryan@picdoctors.com'),
-#   ('Daniel', 'daniel@picdoctors.com'),
+    ('Daniel', 'daniel@picdoctors.com'),
 )
 
 MANAGERS = ADMINS
