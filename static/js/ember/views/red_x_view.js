6c9d786b23aba3beff099a00e42e67809818eae2
kingpin2k <kingpin2k@gmail.com>
2013-09-17 23:54:06 -0600

    I guess this file can go also

---
+++ static/js/ember/views/red_x_view.js
@@ -0,0 +1,35 @@
+Pd.RedXView = Ember.View.extend({
+  tagName: 'img',
+  classNames: ['markup-redx'],
+  attributeBindings: ['src'],
+  src: function(){
+    return Pd.rootUrl + "images/redx.png";
+  }.property(),
+
+
+  mouseDown: function(e){
+    if(e.which === 1){
+      var controller = this.get('controller'),
+          model = controller.get('model'),
+          pic = model.get('pic'),
+          store = controller.get('store');
+      model.deleteRecord();
+      // if it was created this session, it's not automatically deleted
+      // TODO figure out why the hell not
+      if(pic) pic.get('markups').removeObject(model);
+      
+      model.save().then(function(){
+        // worked
+      },
+      function(){
+        // didn't work      
+      });
+    }
+
+    //stop event from propogating up to the pic container
+    return false;
+  }
+
+
+});
+
