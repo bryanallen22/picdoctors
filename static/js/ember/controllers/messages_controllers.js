f54e36ebaeac0367c7c262183defab399056639b
kingpin2k <kingpin2k@gmail.com>
2014-04-04 17:01:53 -0600

    update some random text to be singular/pluralized
    blah blah blah

--- static/js/ember/controllers/messages_controllers.js
+++ static/js/ember/controllers/messages_controllers.js
@@ -2,6 +2,14 @@ Pd.MessagesController = Em.ArrayController.extend({
   openCloseText: function(){
     return this.get('showingComments') ? 'Hide Comments' : 'Show Comments';
   }.property('showingComments'),
+  commentsText: function(){
+    var length = this.get('length'),
+        singular = length === 1,
+        isAre = singular ? 'is' : 'are',
+        comments = singular ? 'comment' : 'comments';
+
+    return 'There ' + isAre + ' ' + length + ' ' + comments;
+  }.property('length'),
   showingComments: false,
 
   actions: {
