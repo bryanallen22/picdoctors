d75486de6482bc053b5327fba00abd4ec17fa9e0
Bryan Allen <bryanallen22@gmail.com>
2014-04-25 01:35:14 -0600

    Show watermarked pic on skaa's album page
    You hadn't quite finished plumbing this in apparently.

--- static/js/ember/controllers/doc_pics_controllers.js
+++ static/js/ember/controllers/doc_pics_controllers.js
@@ -14,7 +14,9 @@ Pd.DocPicGroupController = Ember.ObjectController.extend({
   }.property('controllers.album.isAlbumDoctor'),
 
   visiblePic: function(){
-    return this.get('pic');
+    /* Get the 'pic' if it's available, but it might not be if the user hasn't paid yet.
+     * In that case, grab 'watermark_pic' */
+    return this.get('pic') || this.get('watermark_pic');
   }.property('pic', 'watermark_pic'),
 
   picStyle: function(){
