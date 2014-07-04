304264a440092cf8d58401dcd17baaffb6ddcb1e
kingpin2k <kingpin2k@gmail.com>
2014-07-03 23:15:54 -0600

    pushing the image fix

--- static/js/ember/helpers/handlebar_helper.js
+++ static/js/ember/helpers/handlebar_helper.js
@@ -9,6 +9,6 @@ Ember.Handlebars.helper('capitalize', function(item){
 });
 
 Ember.Handlebars.helper('static_img', function(image){
-  var img = '<img src="' + Pd.staticUrl + image + '">';
-  return Ember.Handlebars.SafeString(img); 
+  var img = '<img src="' + Pd.staticUrl +'images/' + image + '">';
+  return new Ember.Handlebars.SafeString(img); 
 });
