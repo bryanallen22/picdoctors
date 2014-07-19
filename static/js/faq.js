2ff2335087d6ff4377ea08096c30281372581566
kingpin2k <kingpin2k@gmail.com>
2014-07-19 13:19:11 -0600

    faq change

--- static/js/faq.js
+++ static/js/faq.js
@@ -1,9 +1,9 @@
-// Load the application once the DOM is ready, using `jQuery.ready`:
+/*// Load the application once the DOM is ready, using `jQuery.ready`:
 $(function(){
 
   if( $('.faq').length > 0 ) {
 
-    /* If they click a question, fadeIn/fadeOut the answer */
+    // If they click a question, fadeIn/fadeOut the answer 
     $('.question').click( function() {
       var answers = $(this).siblings('.answer');
       for(var i = 0; i < answers.length; i++) {
@@ -19,4 +19,4 @@ $(function(){
 
   }
 
-});
+});*/
