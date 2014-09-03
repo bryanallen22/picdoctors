2493d6396001e584a89dfb530dd000fcf7dd7ffe
kingpin2k <kingpin2k@gmail.com>
2014-09-02 21:13:16 -0600

    add attachment as content-disposition
    when it opens in its own tab it'll download

--- settings/defaults.py
+++ settings/defaults.py
@@ -282,6 +282,7 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 #AWS_CALLING_FORMAT = CallingFormat.SUBDOMAIN
 AWS_HEADERS = {
     #'Expires': 'Thu, 15 Apr 2010 20:00:00 GMT',
+    'Content-Disposition':'attachment;',
     'Cache-Control': 'max-age=86400',
 }
 
